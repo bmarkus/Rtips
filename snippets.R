3e238208f2f038450d3a0e249800543a84a1d806
bmarkus <bmarkus@gmail.com>
2020-10-12 09:23:59 +0300

    Update snippets.R
    melted gt to PCA

--- snippets.R
+++ snippets.R
@@ -1,4 +1,27 @@
+
+# generate PCA from melted (genomics) data
+# -----------------------------------------
+# Assuming input df[marker_key, sample, gt(numeric)]
+
+# Make a matrix with samples in the columns
+data_as_mat <- melted_gt %>% 
+  spread(sample, gt) %>% 
+  column_to_rownames('marker_key') %>% 
+  as.matrix()
+
+# PCA on vcf matrix (sample in columns)
+pca <- prcomp(t(data_as_mat), scale=TRUE, center = TRUE)
+var_explained <- data.frame(PC = 1:length(pca$sdev), percent_var = pca$sdev^2/sum(pca$sdev^2)*100)
+pca_df = as.data.frame(pca$x) %>% tibble::rownames_to_column(var='Sample')
+
+# plot
+pca_df %>% 
+  ggplot(aes(x=PC1, y=PC2, label=Sample))+
+  geom_point()
+
+
 # generate pw distance from PCA coordinates
+# -----------------------------------------
 Dmat <- df %>%
   # assuming a df[FID, PCA1, PCA2, ...] (plink style)
   select(starts_with('PC'),FID) %>% 
