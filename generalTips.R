4639f1b9ba5436ea189c324b3ec64553fde0f6bf
bmarkus <bmarkus@gmail.com>
2020-11-08 15:37:24 +0200

    Update generalTips.R

--- generalTips.R
+++ generalTips.R
@@ -1,3 +1,7 @@
 # Extract single list element as part of a "pipeline"
 # https://community.rstudio.com/t/extract-single-list-element-as-part-of-a-pipeline/1095/5
 # pipe this ... `[[`(1)
+
+# contrasts in R
+# https://rstudio-pubs-static.s3.amazonaws.com/65059_586f394d8eb84f84b1baaf56ffb6b47f.html
+lm(y ~ relevel(cond, ref="C"), data=data)
