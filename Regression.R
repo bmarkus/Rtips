27897246c02e9e24a8d6a978ce2d6be628271f29
bmarkus <bmarkus@gmail.com>
2020-11-11 14:06:51 +0200

    Create Regression.R

---
+++ Regression.R
@@ -0,0 +1,37 @@
+# Linear Models using lm
+# -----------------------
+
+# 3 different ways to get a likelihood ratio test between nested models 
+# Note: alt model includes the null model parameters
+# these are the output of lm.
+# links: 
+# https://api.rpubs.com/tomanderson_34/lrt
+# https://stackoverflow.com/questions/40456314/how-do-i-get-rss-from-a-linear-model-output
+
+alt.df <- alt_model$df.residual
+null.df <- null_model$df.residual
+alt.lik <- logLik(alt_model)
+null.lik <- logLik(null_model)
+test_stat <- -2 * (as.numeric(null.lik) - as.numeric(alt.lik))
+pchisq(test_stat, df = null.df-alt.df, lower.tail = FALSE)
+# 1.712318e-17  
+
+# calculate using the redsiduals
+# should be the same as loglik calculations
+# loglik = -n/2Log(RSS)
+alt.rss <- deviance(alt_model)
+null.rss <- deviance(null_model)
+n=length(resid(null_model))
+pchisq(2 * n/2*log(null.rss/alt.rss), 
+                              null.df - alt.df,
+                              lower.tail = FALSE)
+# 1.712318e-17
+
+anova(null_model, alt_model, test='Chisq') %>% 
+  as_tibble()
+#   Res.Df   RSS    Df `Sum of Sq` `Pr(>Chi)`
+#    <dbl> <dbl> <dbl>       <dbl>      <dbl>
+# 1    237  44.9    NA        NA    NA       
+# 2    234  31.9     3        13.0   1.74e-20
+# slightly lower p value. Why? 
+
